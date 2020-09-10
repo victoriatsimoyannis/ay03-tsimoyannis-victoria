9e384615f372263deb421513f87d3ac012601d0e
Victoria Tsimoyannis <victoriatsimoyannis@MacBook-Air-de-Victoria.local>
2020-09-10 17:06:11 -0300

    entrega

---
+++ java.js
@@ -0,0 +1,19 @@
+
+$(document).ready (function(){
+    $("#boton").click(function(){
+        var idboton = $(this).attr("id");
+        $("#TITULO").text("chao")});
+    
+    $("#texto2").mouseenter(function(){
+        $("#texto2").css("background-color","blue")});
+    
+
+    $("texto3").mouseenter(function(){
+        var idboton = $(this).attr("id");
+        $("texto3").fadeTo("fast",1.5)});
+    })
+  
+
+
+
+   
